d1f13f80ea15199f7d9f1b07399f99425ed95b1d
Muhammad Farouk <muhammadfarouk8428@gmail.com>
2025-10-29 02:11:46 -0700

    edited users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 const { loginUser, signupUser, getAllUsers} = require('../controller/userController')
 
+router.get('/', getAllUsers) // ✅ Now base route returns users
 router.post('/login', loginUser)
-
 router.post('/signup', signupUser)
+router.get('/getusers', getAllUsers) // keep it if you still need it
 
-router.get('/getusers', getAllUsers)
 
 module.exports = router
